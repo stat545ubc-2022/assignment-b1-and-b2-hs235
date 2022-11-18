daba7884b55877e5c48efc1db0e5657a1ac8b222
Harsh Sharma <harsh1792@gmail.com>
2022-11-18 10:43:59 -0800

    updating r file for tags

--- R/func.R
+++ R/func.R
@@ -1,18 +1,19 @@
 #' @title: Summarize by Group Mean
 #'
-#' @description The function automates the calculation of arithmetic mean operating on and summarizing by the grouping chosen by the user
+#' @description The function automates the calculation of arithmetic mean - operating and summarizing on the grouping chosen by the user
 #' 
-#' @details This is a generic function that can group by one or multiple levels passed by the user
-#'          User can pass provide argument for grouping either by numerical index or column name because both ways are used in practice 
-#'          Rows with N/A values are kept as it can still be valid grouping combinations. Thus N/A values are treated via omission in the calculation of mean
+#' @details This is a generic function that can group by one or multiple levels passed by the user.
+#'          User can pass provide argument for grouping either by numerical index or column name because both ways are used in practice.
+#'          Rows with N/A values are kept as it can still be valid grouping combinations. Thus N/A values are treated via omission in the calculation of mean.
 #' 
-#' @param dataFrame dataframe_like_object input only accepts dataframe or tibbular data & hence the chosen name to indicate the inclusion of both
-#' @param group_by vector should be non-empty, can either be numerical or string thus the chosen name is kept generic
+#' @param dataFrame dataframe_like_object accepts either dataframe or tibbular data & hence the chosen name to indicate the inclusion of both
+#' @param group_by vector should be non-empty and can either be numerical or string thus the chosen name is kept generic
 #' 
-#' @return tibble for the case where legal arguments are passed returning values of calculated mean for numeric columns not used in grouping
-#' @return null for the case where invalid arguments are passed to function along with error message   
+#' @return tibble for the case where legal arguments are passed otherwise null along with an error message
+#' 
+#' @examples 
+#' 
 
-#options(dplyr.summarise.inform = FALSE) # Suppressing default message using global option
 #' @export
 summarize_by_group_mean = function(dataFrame, group_by){
   if (! (is.vector(group_by) && length(group_by) > 0)){
